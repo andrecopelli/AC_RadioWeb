5fd84e06582f2537787c06ee73af543d103c62ac
André Copelli <andrecopelli@outlook.com>
2022-12-13 18:54:09 -0300

    Envio dos arquivos do projeto

---
+++ email.php
@@ -0,0 +1,34 @@
+<?php
+
+if(isset($_POST(['email']) && !empty($_POST['email']) {
+
+$nome = addslashes($_POST['name']);
+$email = addslashes($_POST['email']);
+$mensagem = addslashes($_POST['message']);
+
+$to = "ronievon1@gmail.com";
+
+$subject = "Contato - Rádio Serrana";
+
+$body = "Nome: ".$nome."/r/n".
+        "Email: ".$email."/r/n".
+        "Mensagem: ".$mensagem;
+
+$header = "From:andre@probits.com.br"."/r/n".
+          "Reply-To:$email"."/e/n".
+          "X=Mailer:PHP/".phpversion();
+
+if (mail ($to,$subject,$body,$header)) {
+
+    echo("Email enviado com sucesso!");
+
+}else{
+
+    echo("O Email não pode ser enviado!");
+
+}
+
+}
+
+
+?>
